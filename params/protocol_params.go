c36a2586d21f18bd98abc0ac4dcfe6d73c75906d
luomijie <zhiwei-luo@hotmail.com>
2018-01-20 21:45:33 +0800

    modify by roger on 2018-01-19 to 0 fee

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -79,7 +79,7 @@ var (
 	DifficultyBoundDivisor = big.NewInt(2048)                  // The bound divisor of the difficulty, used in the update calculations.
 	GenesisDifficulty      = big.NewInt(131072)                // Difficulty of the Genesis block.
 	MinimumDifficulty      = big.NewInt(131072)                // The minimum that the difficulty may ever be.
-	EthzeroGenesisDifficulty      = big.NewInt(17179860)                // The minimum that the difficulty may ever be.
+	EthzeroGenesisDifficulty      = big.NewInt(1717986918)                // The minimum that the difficulty may ever be.
 	DurationLimit          = big.NewInt(13)                    // The decision boundary on the blocktime duration used to determine whether difficulty should go up or not.
 	DurationEthzeroLimit          = big.NewInt(8)                    // The decision boundary on the blocktime duration used to determine whether difficulty should go up or not.
 
