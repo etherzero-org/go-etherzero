6530a54cf35b0938250c242f752342690804f22d
luomijie <zhiwei-luo@hotmail.com>
2018-01-21 20:51:04 +0800

    modify by roger on 2018-01-21 to add GenesisDifficulty

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -79,8 +79,9 @@ var (
 	DifficultyBoundDivisor = big.NewInt(2048)                  // The bound divisor of the difficulty, used in the update calculations.
 	GenesisDifficulty      = big.NewInt(131072)                // Difficulty of the Genesis block.
 	MinimumDifficulty      = big.NewInt(131072)                // The minimum that the difficulty may ever be.
-	EthzeroGenesisDifficulty      = big.NewInt(1717986918)                // The minimum that the difficulty may ever be.
+	//EthzeroGenesisDifficulty      = big.NewInt(1717986)                // Difficulty of the Etherzero Genesis block.
+	EthzeroGenesisDifficulty      = big.NewInt(17179869184)                // The minimum that the difficulty may ever be.
 	DurationLimit          = big.NewInt(13)                    // The decision boundary on the blocktime duration used to determine whether difficulty should go up or not.
-	DurationEthzeroLimit          = big.NewInt(8)                    // The decision boundary on the blocktime duration used to determine whether difficulty should go up or not.
+	DurationEthzeroLimit          = big.NewInt(13)                    // The decision boundary on the blocktime duration used to determine whether difficulty should go up or not.
 
 	)
