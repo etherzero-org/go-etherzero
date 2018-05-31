6ec8c5c15a535f345e335cde8a30f179533b21f8
luomijie <zhiwei-luo@hotmail.com>
2018-05-31 10:26:44 +0800

    Merge branch 'masternode' of H:\workspace\etzmasternode\src\github.com\ethzero\go-ethzero with conflicts.

--- eth/masternode_manager.go
+++ eth/masternode_manager.go
@@ -22,9 +22,12 @@ import (
 	"encoding/binary"
 	"fmt"
 	"math/big"
+	"math/rand"
 	"net"
 	"sort"
 	"sync"
+
+	"github.com/pkg/errors"
 
 	"github.com/ethzero/go-ethzero/common"
 	"github.com/ethzero/go-ethzero/consensus"
@@ -32,8 +35,6 @@ import (
 	"github.com/ethzero/go-ethzero/core"
 	"github.com/ethzero/go-ethzero/core/types"
 	"github.com/ethzero/go-ethzero/core/types/masternode"
-	"github.com/ethzero/go-ethzero/crypto"
-	"github.com/ethzero/go-ethzero/crypto/secp256k1"
 	"github.com/ethzero/go-ethzero/eth/downloader"
 	"github.com/ethzero/go-ethzero/eth/fetcher"
 	"github.com/ethzero/go-ethzero/ethdb"
@@ -41,9 +42,6 @@ import (
 	"github.com/ethzero/go-ethzero/log"
 	"github.com/ethzero/go-ethzero/p2p"
 	"github.com/ethzero/go-ethzero/params"
-	"github.com/pkg/errors"
-	"math/rand"
-	"time"
 )
 
 const (
@@ -166,7 +164,7 @@ func (self *MasternodeManager) SubscribeWinnerVoteEvent(ch chan<- core.PaymentVo
 
 func (mm *MasternodeManager) newPeer(p *peer) {
 	p.SetMasternode(true)
-	mm.masternodes.SetState(p.id, masternode.MasternodeEnable)
+	mm.masternodes.SetState(p.id, masternode.MasternodeConnected)
 }
 
 // Deterministically select the oldest/best masternode to pay on the network
@@ -176,9 +174,8 @@ func (mm *MasternodeManager) newPeer(p *peer) {
 func (mm *MasternodeManager) GetNextMasternodeInQueueForPayment(block common.Hash) (*masternode.Masternode, error) {
 
 	var (
-		enableNodes  = mm.masternodes.EnableNodes()
 		paids        []int
-		tenthNetWork = len(enableNodes) / 10 // TODO: when len < 10
+		tenthNetWork = mm.masternodes.Len() / 10
 		countTenth   = 0
 		highest      = big.NewInt(0)
 		winner       *masternode.Masternode
@@ -188,8 +185,8 @@ func (mm *MasternodeManager) GetNextMasternodeInQueueForPayment(block common.Has
 	if mm.masternodes == nil {
 		return nil, errors.New("no masternode detected")
 	}
-	fmt.Printf(" GetNextWinner masternodes.nodes %d \n", len(enableNodes))
-	for _, node := range enableNodes {
+	fmt.Printf(" GetNextWinner masternodes.nodes %d \n", len(mm.masternodes.Nodes()))
+	for _, node := range mm.masternodes.Nodes() {
 		i := int(node.Height.Int64())
 		paids = append(paids, i)
 		sortMap[i] = node
@@ -241,7 +238,7 @@ func (mm *MasternodeManager) GetMasternodeScores(blockHash common.Hash, minProto
 
 	masternodeScores := make(map[*big.Int]*masternode.Masternode)
 
-	for _, m := range mm.masternodes.EnableNodes() {
+	for _, m := range mm.masternodes.Nodes() {
 		masternodeScores[m.CalculateScore(blockHash)] = m
 	}
 	return masternodeScores
@@ -282,22 +279,22 @@ func (mm *MasternodeManager) ProcessPaymentVotes(votes []*masternode.MasternodeP
 	return true
 }
 
-func (mm *MasternodeManager) ProcessTxVote(tx *types.Transaction) bool {
+func (mn *MasternodeManager) ProcessTxVote(tx *types.Transaction) bool {
 
-	mm.is.ProcessTxLockRequest(tx)
-	log.Info("Transaction Lock Request accepted,", "txHash:", tx.Hash().String(), "MasternodeId", mm.active.ID)
-	mm.is.Accept(tx)
-	mm.is.Vote(tx.Hash())
+	mn.is.ProcessTxLockRequest(tx)
+	log.Info("Transaction Lock Request accepted,", "txHash:", tx.Hash().String(), "MasternodeId", mn.active.ID)
+	mn.is.Accept(tx)
+	mn.is.Vote(tx.Hash())
 
 	return true
 }
 
 // If server is masternode, connect one masternode at least
-func (mm *MasternodeManager) checkPeers() {
-	if mm.active.State() != masternode.ACTIVE_MASTERNODE_STARTED {
+func (mn *MasternodeManager) checkPeers() {
+	if mn.active.State() != masternode.ACTIVE_MASTERNODE_STARTED {
 		return
 	}
-	for _, p := range mm.peers.peers {
+	for _, p := range mn.peers.peers {
 		if p.isMasternode {
 			return
 		}
@@ -305,8 +302,8 @@ func (mm *MasternodeManager) checkPeers() {
 
 	nodes := make(map[int]*masternode.Masternode)
 	var i int = 0
-	for _, p := range mm.masternodes.EnableNodes() {
-		if p.State == masternode.MasternodeEnable && p.ID != mm.active.ID {
+	for _, p := range mn.masternodes.Nodes() {
+		if p.State == masternode.MasternodeConnected && p.ID != mn.active.ID {
 			nodes[i] = p
 			i++
 		}
@@ -315,85 +312,88 @@ func (mm *MasternodeManager) checkPeers() {
 		return
 	}
 	key := rand.Intn(i - 1)
-	mm.srvr.AddPeer(nodes[key].Node)
+	mn.srvr.AddPeer(nodes[key].Node)
 }
 
-func (mm *MasternodeManager) updateActiveMasternode() {
+func (mn *MasternodeManager) updateActiveMasternode() {
 	var state int
 
-	n := mm.masternodes.Node(mm.active.ID)
+	n := mn.masternodes.Node(mn.active.ID)
 	if n == nil {
 		state = masternode.ACTIVE_MASTERNODE_NOT_CAPABLE
-	} else if int(n.Node.TCP) != mm.active.Addr.Port {
-		log.Error("updateActiveMasternode", "Port", n.Node.TCP, "active.Port", mm.active.Addr.Port)
+	} else if int(n.Node.TCP) != mn.active.Addr.Port {
+		log.Error("updateActiveMasternode", "Port", n.Node.TCP, "active.Port", mn.active.Addr.Port)
 		state = masternode.ACTIVE_MASTERNODE_NOT_CAPABLE
-	} else if !n.Node.IP.Equal(mm.active.Addr.IP) {
-		log.Error("updateActiveMasternode", "IP", n.Node.IP, "active.IP", mm.active.Addr.IP)
+	} else if !n.Node.IP.Equal(mn.active.Addr.IP) {
+		log.Error("updateActiveMasternode", "IP", n.Node.IP, "active.IP", mn.active.Addr.IP)
 		state = masternode.ACTIVE_MASTERNODE_NOT_CAPABLE
 	} else {
 		state = masternode.ACTIVE_MASTERNODE_STARTED
 	}
 
-	mm.active.SetState(state)
+	mn.active.SetState(state)
 }
-func (mm *MasternodeManager) masternodeLoop() {
-	mm.updateActiveMasternode()
-	if mm.active.State() == masternode.ACTIVE_MASTERNODE_STARTED {
+func (mn *MasternodeManager) masternodeLoop() {
+	mn.updateActiveMasternode()
+	if mn.active.State() == masternode.ACTIVE_MASTERNODE_STARTED {
 		fmt.Println("masternodeCheck true")
-		mm.checkPeers()
-	} else if !mm.srvr.MasternodeAddr.IP.Equal(net.IP{}) {
+		mn.checkPeers()
+	} else if !mn.srvr.MasternodeAddr.IP.Equal(net.IP{}) {
 		var misc [32]byte
 		misc[0] = 1
-		copy(misc[1:17], mm.srvr.Config.MasternodeAddr.IP)
-		binary.BigEndian.PutUint16(misc[17:19], uint16(mm.srvr.Config.MasternodeAddr.Port))
+		copy(misc[1:17], mn.srvr.Config.MasternodeAddr.IP)
+		binary.BigEndian.PutUint16(misc[17:19], uint16(mn.srvr.Config.MasternodeAddr.Port))
 
 		var buf bytes.Buffer
-		buf.Write(mm.srvr.Self().ID[:])
+		buf.Write(mn.srvr.Self().ID[:])
 		buf.Write(misc[:])
 		d := "0x4da274fd" + common.Bytes2Hex(buf.Bytes())
 		fmt.Println("Masternode transaction data:", d)
 	}
 
-	mm.masternodes.Show()
+	mn.masternodes.Show()
 
 	joinCh := make(chan *contract.ContractJoin, 32)
 	quitCh := make(chan *contract.ContractQuit, 32)
-	joinSub, err1 := mm.contract.WatchJoin(nil, joinCh)
+	joinSub, err1 := mn.contract.WatchJoin(nil, joinCh)
 	if err1 != nil {
 		// TODO: exit
 		return
 	}
-	quitSub, err2 := mm.contract.WatchQuit(nil, quitCh)
+	quitSub, err2 := mn.contract.WatchQuit(nil, quitCh)
 	if err2 != nil {
 		// TODO: exit
 		return
 	}
 
-	ping := time.NewTimer(masternode.MASTERNODE_PING_INTERVAL)
-	check := time.NewTimer(masternode.MASTERNODE_CHECK_INTERVAL)
+	//pingMsg := &masternode.PingMsg{
+	//	ID: self.node.ID,
+	//	IP: self.node.IP,
+	//	Port: self.node.TCP,
+	//}
+	//t := time.NewTimer(time.Second * 5)
 
 	for {
 		select {
 		case join := <-joinCh:
 			fmt.Println("join", common.Bytes2Hex(join.Id[:]))
-			node, err := mm.masternodes.NodeJoin(join.Id)
+			node, err := mn.masternodes.NodeJoin(join.Id)
 			if err == nil {
-				if bytes.Equal(join.Id[:], mm.srvr.Self().ID[0:32]) {
-					mm.updateActiveMasternode()
-					ping.Reset(masternode.MASTERNODE_PING_INTERVAL)
+				if bytes.Equal(join.Id[:], mn.srvr.Self().ID[0:32]) {
+					mn.updateActiveMasternode()
 				} else {
-					mm.srvr.AddPeer(node.Node)
+					mn.srvr.AddPeer(node.Node)
 				}
-				mm.masternodes.Show()
+				mn.masternodes.Show()
 			}
 
 		case quit := <-quitCh:
 			fmt.Println("quit", common.Bytes2Hex(quit.Id[:]))
-			mm.masternodes.NodeQuit(quit.Id)
-			if bytes.Equal(quit.Id[:], mm.srvr.Self().ID[0:32]) {
-				mm.updateActiveMasternode()
+			mn.masternodes.NodeQuit(quit.Id)
+			if bytes.Equal(quit.Id[:], mn.srvr.Self().ID[0:32]) {
+				mn.updateActiveMasternode()
 			}
-			mm.masternodes.Show()
+			mn.masternodes.Show()
 
 		case err := <-joinSub.Err():
 			joinSub.Unsubscribe()
@@ -402,47 +402,16 @@ func (mm *MasternodeManager) masternodeLoop() {
 			quitSub.Unsubscribe()
 			fmt.Println("eventQuit err", err.Error())
 
-		case <-ping.C:
-			if mm.active.State() != masternode.ACTIVE_MASTERNODE_STARTED {
-				continue
-			}
-			msg, err := mm.active.NewPingMsg()
-			if err != nil {
-				log.Error("NewPingMsg", "error", err)
-				continue
-			}
-			peers := mm.peers.peers
-			for _, peer := range peers {
-				log.Debug("sending ping msg", "peer", peer.id)
-				if err := peer.SendMasternodePing(msg); err != nil {
-					log.Error("SendMasternodePing", "error", err)
-				}
-			}
-			ping.Reset(masternode.MASTERNODE_PING_INTERVAL)
-
-		case <-check.C:
-			mm.masternodes.Check()
-			check.Reset(masternode.MASTERNODE_CHECK_INTERVAL)
+			//case <-t.C:
+			//	pingMsg.Update(self.privateKey)
+			//	peers := self.peers.peers
+			//	for _, peer := range peers {
+			//		fmt.Println("peer", peer.ID())
+			//		if err := peer.SendMasternodePing(pingMsg); err != nil {
+			//			fmt.Println("err:", err)
+			//		}
+			//	}
+			//	t.Reset(time.Second * 100)
 		}
-
 	}
-}
-
-func (mm *MasternodeManager) DealPingMsg(pm *masternode.PingMsg) error {
-	var b [8]byte
-	binary.BigEndian.PutUint64(b[:], pm.Time)
-	key, err := secp256k1.RecoverPubkey(crypto.Keccak256(b[:]), pm.Sig)
-	if err != nil || len(key) != 65 {
-		return err
-	}
-	id := fmt.Sprintf("%x", key[1:9])
-	node := mm.masternodes.Node(id)
-	if node == nil {
-		return fmt.Errorf("error id %s", id)
-	}
-	if node.LastPingTime > pm.Time {
-		return fmt.Errorf("error ping time: %d > %d", node.LastPingTime, pm.Time)
-	}
-	mm.masternodes.RecvPingMsg(id, pm.Time)
-	return nil
 }
--- eth/protocol.go
+++ eth/protocol.go
@@ -37,7 +37,7 @@ const (
 )
 
 // Official short name of the protocol used during capability negotiation.
-var ProtocolName = "etz"
+var ProtocolName = "ett"
 
 // Supported versions of the eth protocol (first is primary).
 var ProtocolVersions = []uint{etz64, eth63, eth62}
