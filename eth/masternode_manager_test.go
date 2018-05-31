74d2793544a92731d6925b43aebb30ce3a175308
luomijie <zhiwei-luo@hotmail.com>
2018-05-31 09:39:18 +0800

    Increase vote event and forwarding function

---
+++ eth/masternode_manager_test.go
@@ -0,0 +1,21 @@
+package eth
+
+
+import (
+
+	"testing"
+)
+
+
+func TestMasternodeManager_GetNextMasternodeInQueueForPayment(t *testing.T) {
+
+}
+
+
+func TestMasternodeManager_GetMasternodeScores(t *testing.T) {
+
+}
+
+func TestMasternodeManager_GetMasternodeRank(t *testing.T) {
+
+}
