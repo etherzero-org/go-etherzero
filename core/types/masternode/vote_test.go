d37d337e1fdd77199991a96697964d79440ec246
dylanpoe <1121140449@qq.com>
2018-06-02 18:09:23 +0800

    optimize test files core/types/masternode/vote_test.go

--- core/types/masternode/vote_test.go
+++ core/types/masternode/vote_test.go
@@ -8,17 +8,13 @@ import (
 )
 
 func TestTxLockVote_IsValid(t *testing.T) {
-	//var _startDate int64 = time.Now().Unix()             //+ int64(time.Second*10)
-	//var createdTime time.Time = time.Unix(_startDate, 0) //.Format("2006-01-02 15:04:05")
-	//fmt.Printf("%v\n", uint64(time.Now().Sub(createdTime)))
-	//fmt.Printf("%v\n", time.Now().Sub(createdTime))
 	var hash common.Hash
 	for i := range hash {
 		hash[i] = byte(i)
 	}
 	txLockVote := NewTxLockVote(
 		hash,
-		fmt.Sprintf("aaa"),
+		fmt.Sprintf("0x65bc97ef01b35f86a45c319675a05699e0947743c59ed53d0a918fb215c5ee5f"),
 	)
 	fmt.Printf("timeout ret is %v", txLockVote.IsTimeOut())
 }
