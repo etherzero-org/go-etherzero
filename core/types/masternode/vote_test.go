bdebae80d1447703b6e88d741d0dcd4bca031f99
dylanpoe <1121140449@qq.com>
2018-06-02 17:34:54 +0800

    modify definations about  TimeoutSeconds in params/protocol_params.go and add vote_test.go

---
+++ core/types/masternode/vote_test.go
@@ -0,0 +1,24 @@
+package masternode
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ethzero/go-ethzero/common"
+)
+
+func TestTxLockVote_IsValid(t *testing.T) {
+	//var _startDate int64 = time.Now().Unix()             //+ int64(time.Second*10)
+	//var createdTime time.Time = time.Unix(_startDate, 0) //.Format("2006-01-02 15:04:05")
+	//fmt.Printf("%v\n", uint64(time.Now().Sub(createdTime)))
+	//fmt.Printf("%v\n", time.Now().Sub(createdTime))
+	var hash common.Hash
+	for i := range hash {
+		hash[i] = byte(i)
+	}
+	txLockVote := NewTxLockVote(
+		hash,
+		fmt.Sprintf( "aaa"),
+	)
+	fmt.Printf("%v", txLockVote.IsTimeOut())
+}
