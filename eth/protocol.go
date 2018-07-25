c9eca73aabea8455f912761b9faaf32b7885314e
luomijie <zhiwei-luo@hotmail.com>
2018-07-25 20:32:48 +0800

    add protocol version

--- eth/protocol.go
+++ eth/protocol.go
@@ -36,7 +36,7 @@ const (
 )
 
 // ProtocolName is the official short name of the protocol used during capability negotiation.
-var ProtocolName = "etz"
+var ProtocolName = "etz0725"
 
 // ProtocolVersions are the upported versions of the eth protocol (first is primary).
 var ProtocolVersions = []uint{etz64, eth63, eth62}
