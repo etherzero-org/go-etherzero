19cb1ec0283ce7a7f472ac97baee4c84329aa84d
Rolong <rolong@vip.qq.com>
2018-08-17 15:07:42 +0800

    Print witness when imported new chain segment

--- core/blockchain.go
+++ core/blockchain.go
@@ -1256,6 +1256,7 @@ func (st *insertStats) report(chain []*types.Block, index int, cache common.Stor
 			"blocks", st.processed, "txs", txs, "mgas", float64(st.usedGas) / 1000000,
 			"elapsed", common.PrettyDuration(elapsed), "mgasps", float64(st.usedGas) * 1000 / float64(elapsed),
 			"number", end.Number(), "hash", end.Hash(), "cache", cache,
+			"witness", end.Header().Witness,
 		}
 		if st.queued > 0 {
 			context = append(context, []interface{}{"queued", st.queued}...)
