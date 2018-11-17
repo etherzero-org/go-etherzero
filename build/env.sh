4bf7032e89ca9ba1869ec257d8400436b4fc26b3
dylanpoe <1121140449@qq.com>
2018-11-17 17:41:08 +0800

    fix build/env.sh

--- build/env.sh
+++ build/env.sh
@@ -10,11 +10,11 @@ fi
 # Create fake Go workspace if it doesn't exist yet.
 workspace="$PWD/build/_workspace"
 root="$PWD"
-ethdir="$workspace/src/github.com/ethereum"
-if [ ! -L "$ethdir/go-ethereum" ]; then
+ethdir="$workspace/src/github.com/etherzero"
+if [ ! -L "$ethdir/go-etherzero" ]; then
     mkdir -p "$ethdir"
     cd "$ethdir"
-    ln -s ../../../../../. go-ethereum
+    ln -s ../../../../../. go-etherzero
     cd "$root"
 fi
 
@@ -23,8 +23,8 @@ GOPATH="$workspace"
 export GOPATH
 
 # Run the command inside the workspace.
-cd "$ethdir/go-ethereum"
-PWD="$ethdir/go-ethereum"
+cd "$ethdir/go-etherzero"
+PWD="$ethdir/go-etherzero"
 
 # Launch the arguments with the configured environment.
 exec "$@"
