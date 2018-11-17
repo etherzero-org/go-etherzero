8a1dc25eb7331250e5c9d5a96d7f68fa34fd4657
Rolong <rolong@vip.qq.com>
2018-11-17 16:09:40 +0800

    Setting default ports and data folder

--- node/defaults.go
+++ node/defaults.go
@@ -29,9 +29,9 @@ import (
 
 const (
 	DefaultHTTPHost = "localhost" // Default host interface for the HTTP RPC server
-	DefaultHTTPPort = 8545        // Default TCP port for the HTTP RPC server
+	DefaultHTTPPort = 9646        // Default TCP port for the HTTP RPC server
 	DefaultWSHost   = "localhost" // Default host interface for the websocket RPC server
-	DefaultWSPort   = 8546        // Default TCP port for the websocket RPC server
+	DefaultWSPort   = 9647        // Default TCP port for the websocket RPC server
 )
 
 // DefaultConfig contains reasonable default settings.
@@ -44,7 +44,7 @@ var DefaultConfig = Config{
 	WSPort:           DefaultWSPort,
 	WSModules:        []string{"net", "web3"},
 	P2P: p2p.Config{
-		ListenAddr: ":30303",
+		ListenAddr: ":21212",
 		MaxPeers:   25,
 		NAT:        nat.Any(),
 	},
@@ -57,11 +57,11 @@ func DefaultDataDir() string {
 	home := homeDir()
 	if home != "" {
 		if runtime.GOOS == "darwin" {
-			return filepath.Join(home, "Library", "Ethereum")
+			return filepath.Join(home, "Library", "Etherzero")
 		} else if runtime.GOOS == "windows" {
-			return filepath.Join(home, "AppData", "Roaming", "Ethereum")
+			return filepath.Join(home, "AppData", "Roaming", "Etherzero")
 		} else {
-			return filepath.Join(home, ".ethereum")
+			return filepath.Join(home, ".etherzero")
 		}
 	}
 	// As we cannot guess a stable location, return empty and handle later
