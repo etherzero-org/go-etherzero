d3bf459288c6eb320628d36da0740f50e49eaf77
dylanpoe <1121140449@qq.com>
2018-12-22 11:09:47 +0800

    eth/protocol.go

--- eth/protocol.go
+++ eth/protocol.go
@@ -32,16 +32,18 @@ import (
 const (
 	eth62 = 62
 	eth63 = 63
+	etz64 = 64
 )
 
 // ProtocolName is the official short name of the protocol used during capability negotiation.
-var ProtocolName = "etz01"
 
-// ProtocolVersions are the supported versions of the eth protocol (first is primary).
-var ProtocolVersions = []uint{eth63, eth62}
+var ProtocolName = "etz"
+
+// ProtocolVersions are the upported versions of the eth protocol (first is primary).
+var ProtocolVersions = []uint{etz64, eth63, eth62}
 
 // ProtocolLengths are the number of implemented message corresponding to different protocol versions.
-var ProtocolLengths = []uint64{17, 8}
+var ProtocolLengths = []uint64{35, 17, 8}
 
 const ProtocolMaxMsgSize = 10 * 1024 * 1024 // Maximum cap on the size of a protocol message
 
@@ -67,7 +69,7 @@ const (
 type errCode int
 
 const (
-	ErrMsgTooLarge = iota
+	ErrMsgTooLarge             = iota
 	ErrDecode
 	ErrInvalidMsgCode
 	ErrProtocolVersionMismatch
