6e1eca5b0504b16ff4ef6af71893d108d98f164b
Rolong <rolong@vip.qq.com>
2019-06-17 16:13:09 +0800

    Update version.go

--- params/version.go
+++ params/version.go
@@ -21,9 +21,9 @@ import (
 )
 
 const (
-	VersionMajor = 0        // Major version component of the current release
-	VersionMinor = 3        // Minor version component of the current release
-	VersionPatch = 8        // Patch version component of the current release
+	VersionMajor = 2        // Major version component of the current release
+	VersionMinor = 0        // Minor version component of the current release
+	VersionPatch = 9        // Patch version component of the current release
 	VersionMeta  = "unstable" // Version metadata to append to the version string
 )
 
