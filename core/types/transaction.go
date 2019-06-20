72156151bc615cd2e5103bb1b90906c31a07de89
dylanpoe <1121140449@qq.com>
2019-06-20 18:51:29 +0800

    comment total.Add(total, tx.data.Amount)

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -246,9 +246,10 @@ func (tx *Transaction) WithSignature(signer Signer, sig []byte) (*Transaction, e
 }
 
 // Cost returns amount + gasprice * gaslimit.
+// why
 func (tx *Transaction) Cost() *big.Int {
 	total := new(big.Int).Mul(tx.data.Price, new(big.Int).SetUint64(tx.data.GasLimit))
-	total.Add(total, tx.data.Amount)
+	// total.Add(total, tx.data.Amount)
 	return total
 }
 
