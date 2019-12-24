c5b30db18ed6e52bc5b8139862ba9a92c602d2a7
luomijie <zhiwei-luo@hotmail.com>
2019-12-24 17:05:19 +0800

    add local node

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -17,7 +17,7 @@
 package params
 
 var MainnetBootnodes = []string{
-	"enode://3b9471c1b4d93a45a1f7aff368d027dc7eeac7c526d80848d9848773b0426f41931ecdafa6f513800f68b5425f5b1a482ccbd6eb4b0f39982c4d3ff0cefe085e@35.182.48.79:21212", // Canada
+	"enode://6c3f8db1fbd61497c4a2ecc43bd6e78ad4d23255ef1aa0cbf4793deeb7b333c20d601db88e83878ac9b36c9d7d98041f7eb4c8669a45e19cb2662828fc9b0ca6@127.0.0.1:30303", // Canada
 	"enode://c4fed9426ad1355c845edbc6442ef6bc3e9edb9eacdc42f613c297e8b986dc8099956a1b79c3d2ad12118d77e20c71fc22e2876773d5508cd0d7707b91767e35@52.47.202.205:21212", // Paris
 	"enode://33e3f4ea45c3b20d1703be686ce6f6e1726fe6279451eda864a8e63d57324c89ebe3ba0167c457850e50a9ec153e063e4f4ff6344c821db93d06c0ab87d092f5@3.0.240.110:21212", // Singapore
 	"enode://3e5655447985a71d2c46097fe790c95310a7d075f7333ed3019897cbbde057624f31285999da192aa27ac48f801d24bbb61fac5bf1bacd30439018843d676e59@13.52.109.169:21212", // California
@@ -26,7 +26,7 @@ var MainnetBootnodes = []string{
 // MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
 // the main Ethereum network.
 var MainnetMasternodes = []string{
-	"enode://601d482e243e16ee7387d5f3b814f9dc233853ebf08f0a7f110417a21bc23c7bf71f94adfa578d90889450015f8ea4dd07825886635efa506d10751f1080f882", // [1]
+	"enode://6c3f8db1fbd61497c4a2ecc43bd6e78ad4d23255ef1aa0cbf4793deeb7b333c20d601db88e83878ac9b36c9d7d98041f7eb4c8669a45e19cb2662828fc9b0ca6", // [1]
 	"enode://190adef951323157d8e1024fc685429599c03e05f1cd62ccd74bd82afaca78a94e80bc92e17227460a9e2126f45097204fb32d67373737017f0dfd348d230abc", // [2]
 }
 
@@ -35,7 +35,7 @@ var TestnetMasternodes = []string{
 }
 
 var MainnetInitIds = []string{
-	"601d482e243e16ee",
+	"6c3f8db1fbd61497",
 	"041515e0266f1265",
 	"f5ad3fe12339603f",
 	"550c6eee655f1893",
